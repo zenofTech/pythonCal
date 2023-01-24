18ab52891578dc1a2cf2d6e0222e4a47e69521c8
zenoftech <chosentechnologie@gmail.com>
2023-01-24 22:46:36 +0100

    initial commit

---
+++ cal.py
@@ -0,0 +1,24 @@
+"""
+Created by: ZenOfTech
+Date: 23-01-2023
+Email: amuktar064@gmail.com
+site: amuktar.github.io/muktar
+"""
+
+# This print function is to print the coder
+print("\n","\t"*4,"Created","\n","\t"*3,"\\"*25, "\n","\t"*4,"   By","\n","\t"*4,"Zenoftech\n", "\t"*3,"\\"*25)
+
+# Here are the instructions the user to follow
+print("""
+    Use the arithemetic signs below for your calculation:
+    +   For addition
+    -   For subtraction
+    *   For multiplication
+    /   For division
+    //  For integer (division without decimal point value)
+    %   For Modulus
+
+    """)
+
+
+
